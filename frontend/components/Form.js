7d36d61df1512e15c17e88e5ad56d6fb882d8252
Sterling Sanders <sterlingsanders723@gmail.com>
2023-08-31 16:17:04 -0700

    all done

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,32 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  state = {
+    name: '',
+  }
+
+  onSubmit = evt => {
+    evt.preventDefault();
+    this.props.addTodo(this.state.name)
+    this.setState({
+      name: ''
+    })
+  }
+
+  onChange = evt => {
+    this.setState({
+      name: evt.target.value
+    })
+  }
+
   render() {
+    const { addTodo } = this.props
     return (
-      <div>
-        Form
-      </div>
+      <form onSubmit={this.onSubmit}>
+        <input type='text' value={this.state.name} onChange={this.onChange} />
+        <input type='submit' />
+
+      </form>
     )
   }
 }
